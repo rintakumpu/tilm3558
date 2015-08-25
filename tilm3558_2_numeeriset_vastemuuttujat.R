45dee5c0d57568ec1f64c6dc9ff60925d535de00
Lasse Rintakumpu <rintakumpu@gmail.com>
2015-08-25 19:57:00 +0300

    Add rest of part 2

--- tilm3558_2_numeeriset_vastemuuttujat.R
+++ tilm3558_2_numeeriset_vastemuuttujat.R
@@ -1,7 +1,7 @@
 #########################################
 # TILM3558 Harjoitustyö, Osa 2, R-koodi #
 # Lasse Rintakumpu, 63555               #
-# 19.8.2015                             #
+# 25.8.2015                             #
 #########################################
 
 # Asetetaan työhakemisto
@@ -16,7 +16,7 @@ lataa_kirjasto <- function(kirjasto) {
 }
 
 # Ladataan/asennetaan käytetyt kirjastot
-lapply(c("moments", "car", "ARTool"), lataa_kirjasto)
+lapply(c("moments", "car", "ARTool", "corrplot", "MASS"), lataa_kirjasto)
 
 # Ladataan havaintoaineisto
 elinolo <- read.csv("https://raw.githubusercontent.com/rintakumpu/tilm3558/master/elinolo_filtered.csv", sep=",", dec=".", header=TRUE, row.names=NULL, fileEncoding = "UTF-8-BOM");
@@ -152,3 +152,99 @@ anova(art(pala~supu+kumu+supu:kumu))
 # supu       842829  1    996 10.2084  0.001442 ** 
 # kumu      2312695  1    996 28.4415 1.195e-07 ***
 # supu:kumu    4186  1    996  0.0501  0.822976 
+
+#################################################
+# 1b. Mallin oletusten ja muuttujien tarkastelu #
+#################################################
+
+# Ladataan data omiin muuttujiinsa
+elinolo <- read.csv("https://raw.githubusercontent.com/rintakumpu/tilm3558/master/elinolo_filtered_b.csv", sep=",", dec=".", header=TRUE, row.names=NULL, fileEncoding = "UTF-8-BOM");
+rkkot <- elinolo$rkkot[elinolo$filter==1] # Kotitalouden tulot, euroa per vuosi?
+asmenot <- elinolo$asmenot[elinolo$filter==1] # Asumismenot, euroa per vuosi?
+alaika <- elinolo$alaika[elinolo$filter==1] # Alueella asumisaika, kuukautta?
+
+# Ladataan data matriisiin
+elinolo_matrix <- matrix(c(pala,rkkot,asmenot,alaika), ncol=4, byrow=FALSE, 
+                         dimnames=list(c(1:1000),c("pala", "rkkot", "asmenot", "alaika" )))
+
+ 
+# Piirretään sirontakuvio
+scatterplotMatrix(~pala+rkkot+asmenot+alaika, data=elinolo_matrix)
+pdf ('scatterplot_pala_rkkot_asmenot_alaika.pdf')
+dev.off()
+
+# Piirretään korrelaatiomatrisi
+corrplot(cor(elinolo_matrix, method="pearson"), method="number", col="black", type="upper", tl.pos="tl", diag=TRUE, cl.pos="n", tl.col="black")
+corrplot(cor(elinolo_matrix, method="spearman"), col="#777777", add=TRUE, method="number", type="lower", tl.pos="n", cl.pos="n", tl.col="black", diag=FALSE)
+
+pdf('corrplot_pala_rkkot_asmenot_alaika.pdf')
+dev.off()
+
+# Riippuvuus vain palan ja rkkotin välillä lineaarista, Rs > Rp. 
+# Jakaumat eivät normaaleja, sovelletaan muunnoksia dataan
+
+pala_pt <- powerTransform(pala) # 0.3128892
+rkkot_pt <- powerTransform(rkkot) # 0.5585493
+
+elinolo_matrix_transform <- matrix(c(pala^0.3128892,rkkot^0.5585493, asmenot,alaika), ncol=4, byrow=FALSE, 
+                         dimnames=list(c(1:1000),c("pala", "rkkot", "asmenot", "alaika" ))
+
+cor(elinolo_matrix_transform[,"pala"],elinolo_matrix_transform[,"rkkot"], method="pearson")
+# 0.5640465
+cor(elinolo_matrix_transform[,"pala"],elinolo_matrix_transform[,"rkkot"], method="spearman")
+# 0.5567287
+
+######################
+# 2b. Mallin valinta #
+######################
+
+malli1 <- lm(pala~rkkot+asmenot+alaika, data=as.data.frame(elinolo_matrix_transform))
+summary(stepAIC(malli1, direction="backward"))
+malli2 <- lm(pala~rkkot+alaika, data=as.data.frame(elinolo_matrix_transform))
+summary(stepAIC(malli2, direction="backward"))
+
+# Coefficients:
+# Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) 3.1764747  0.0497292   63.88  < 2e-16 ***
+#  rkkot       0.0017090  0.0000750   22.79  < 2e-16 ***
+#  alaika      0.0053713  0.0007841    6.85 1.29e-11 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+# Residual standard error: 0.4016 on 997 degrees of freedom
+# Multiple R-squared:  0.3488,  Adjusted R-squared:  0.3475 
+# F-statistic:   267 on 2 and 997 DF,  p-value: < 2.2e-16
+
+# Eteenpäin askeltaen
+malli3 <- lm(pala~rkkot, data=as.data.frame(elinolo_matrix_transform))
+malli4 <- lm(pala~rkkot+asmenot, data=as.data.frame(elinolo_matrix_transform))
+summary(stepAIC(malli3, direction="forward"))
+summary(stepAIC(malli4, direction="forward"))
+summary(stepAIC(malli2, direction="forward")) # Päädytään samaan malliin
+
+summary(malli3)
+
+# Residual standard error: 0.4107 on 998 degrees of freedom
+# Multiple R-squared:  0.3181,  Adjusted R-squared:  0.3175 
+# F-statistic: 465.7 on 1 and 998 DF,  p-value: < 2.2e-16
+
+# Jatketaan mallilla 2
+coefficients(malli2)
+malli <- malli2
+# 3.176474690 0.001708954 0.005371325 
+# Regressioyhtälö:
+# pala=rkkot*0.0017+alaika*0.0054+3.1765.
+
+############################################
+# 3b. Mallin jatkotarkastelu ja yhteenveto #
+############################################
+
+# Tarkastellaan valitun mallin (malli3) jäännöksiä
+par(mfrow=c(2,2))
+
+# Jäännösten histogrammi
+hist(rstandard(malli), freq=FALSE, main="", ylab="", xlab="Standardized residuals")
+xfit<-seq(min(rstandard(malli)),max(rstandard(malli)),length=40) 
+yfit<-dnorm(xfit) 
+lines(xfit, yfit)
+plot(malli)
